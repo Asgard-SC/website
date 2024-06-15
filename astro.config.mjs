80573505acb4e2d5d8b0b8dd1ec7f1894693e1a9
Nabel <83960299+cnabel@users.noreply.github.com>
2024-06-15 23:23:01 +0200

    Change from .today to .sc

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue(), sitemap()],
-  site: 'https://asgard.today',
+  site: 'https://asgard.sc',
 });
